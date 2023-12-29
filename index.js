855063947ccf63146e25949f9def1c63b4819685
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2023-12-29 10:49:13 +0600

    Function using array destructuring in the parameter list

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-function fun(...x) {
-  x.forEach((e, i) => {
-    console.log(i, e);
-  });
+function sum([...arr]) {
+  console.log(arr);
 }
-let arr1 = [0, 1, 2];
-const arr2 = [3, 4, 5];
-fun(arr1, arr2);
+let numbers = [1, 2, 3, 4];
+const result = sum(numbers);
