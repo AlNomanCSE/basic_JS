fd5ed21d9036a320092127fc5505b481cb5ae401
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-01-09 20:25:23 +0600

    after two days

--- index.js
+++ index.js
@@ -3,18 +3,13 @@
 // }
 // let numbers = [1, 2, 3, 4];
 // const result = sum(numbers);
-
 // function createGreeter(greeting) {
 //   return function (name) {
 //     console.log(`${greeting}, ${name}!`);
 //   };
 // }
-
 // const customGreet = createGreeter("Screw");
-
 // customGreet("ScrewY");
 
-let str = "123";
-console.log(typeof str);
-let num = Number(str);
-console.log(num);
+let age = Number("abdullah al Noman");
+console.log(age);
