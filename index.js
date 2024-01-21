4f6df2c675951bfffe3d2072683a3039184b0a6c
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-01-21 18:34:07 +0600

    call() apply() bind()

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "./user.json", true);
+// xhr.open("GET", "./user.json", true);
 
-xhr.onload = function () {
-  if (this.status == 200) {
-    let myData = JSON.parse(this.responseText);
-    console.log(myData);
-  }
+// xhr.onload = function () {
+//   if (this.status == 200) {
+//     let myData = JSON.parse(this.responseText);
+//     console.log(myData);
+//   }
+// };
+// xhr.send();
+
+let person = {
+  firstName: "Jhon",
+  lastName: "Smith",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
-xhr.send();
+let p1 = {
+  firstName: "XNJ",
+  lastName: "SmSDith",
+};
+
+console.log(person.fullName.call(p1));
+let definePerson = function (age, job) {
+  console.log(this.fullName() + " is " + age + " : " + job);
+};
+// definePerson.call(person, 25, "FrontEnd Developer");
+// definePerson.apply(person, [25, "FrontEnd Developer"]);
+
+let getPerson = definePerson.bind(person,25);
+// getPerson( 27, "IT");
+// definePerson.bind(person, 25, "IT")()
+getPerson("production");
